627990f6fc2bc833d3cd29ee44deef88ee32682d
Thea Peñaflorida <133612323+itstheadesu@users.noreply.github.com>
2023-09-08 10:07:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// add function
 function displaySum() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
@@ -7,3 +8,15 @@ function displaySum() {
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
+
+
+// subtract function
+function displayDiff() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('subButton').addEventListener("click", displayDiff);
